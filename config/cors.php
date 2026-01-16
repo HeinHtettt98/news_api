03d57250ef07e8079d8243496081eaac7dfad647
HeinHtettt98 <heinhtet352002@gmail.com>
2026-01-16 14:11:38 +0630

    cors_update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173',"https://vercel.com/heins-projects-33378e71/laravel-news-app"],
 
     'allowed_origins_patterns' => [],
 
@@ -32,3 +32,5 @@ return [
     'supports_credentials' => true,
 
 ];
+
+
